8b138e0e110085c3eed1d5f6ef60b1524460915c
jsdeemon <jsdeemon@gmail.com>
2023-08-10 17:49:40 +0500

    added some files

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './pages/cart/cart.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'cart',
+    component: CartComponent
+  },
+  {
+    path: '', redirectTo: 'home', pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
+// import { NgModule } from '@angular/core';
+// import { RouterModule, Routes } from '@angular/router';
+
+// const routes: Routes = [];
+
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
---
+++ src/app/services/store.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Product } from 'src/app/models/product.model';
+
+const FAKE_STORE_API = 'https://fakestoreapi.com';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService {
+
+  constructor(private httpClient: HttpClient) { }
+
+  getAllProducts(limit = '12', sort = 'desc'): Observable<Array<Product>> {
+    return this.httpClient.get<Array<Product>>(
+      `${FAKE_STORE_API}/products?sort=${sort}&limit=${limit}`
+    )
+  }
+
+  getAllCategories(): Observable<Array<string>> {
+    return this.httpClient.get<Array<string>>(
+      `${FAKE_STORE_API}/products/categories`
+    )
+  }
+
+}
