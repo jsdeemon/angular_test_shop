2c2cb04e0374aec8fb561083c73585942db182f5
jsdeemon <jsdeemon@gmail.com>
2023-08-11 21:05:23 +0500

    added interval

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -7,7 +7,7 @@ import { StoreService } from 'src/app/services/store.service';
 
 import { HostListener } from '@angular/core';
 
-
+import { interval } from 'rxjs';
 
 const ROWS_HEIGHT: {[id: number]: number} = {1: 400, 3: 335, 4: 350};
 
@@ -25,40 +25,15 @@ export class HomeComponent implements OnInit, OnDestroy {
   sort = 'desc';
   count = '1';
   prev: number = 0;
-  items = [];
   productsSubscription: Subscription | undefined;
-  product = {}
+
 
 
  
 
   constructor(private cartService: CartService, private storeService: StoreService) { }
 
-  // @HostListener("window:scroll", []) onWindowScroll() {
-  //   // do some stuff here when the window is scrolled
-  //   // нужно добавить число, сложить с числом и потом перевести в тип строки
-    
-  //  // this.count = (+this.count + 1).toString();
-  //  this.prev = Number(this.count) + 3
-  //  this.count = this.prev.toString();
-  //    this.getProducts()
-  
-    
-  //   // this.products = this.products?.slice(0,2);
-  //   const verticalOffset = window.pageYOffset 
-  //         || document.documentElement.scrollTop 
-  //         || document.body.scrollTop || 0;
-  // }
 
-  @HostListener('window:scroll')
-onWindowScroll() {
-    console.log("scrolling...");
-       this.count = (+this.count + 1).toString();
-   this.prev = Number(this.count) + 3
-    this.count = this.prev.toString();
-    console.log(this.count)
-     // this.getProducts()
-}
 
   ngOnInit(): void {
     try {
@@ -72,18 +47,39 @@ onWindowScroll() {
 
 
 
-  getProducts(): void {
+   getProducts(): void {
     
       // this.productsSubscription = this.storeService.getAllProducts(this.count, this.sort).subscribe((_products) => {
       //   this.products =  _products;
       // });
 
-      this.productsSubscription = this.storeService.getLimitedProducts(this.count).subscribe((_products) => {
+       this.productsSubscription = this.storeService.getLimitedProducts(this.count).subscribe((_products) => {
         this.products =  _products;
       });
   }
 
-
+  currentPosition = window.pageYOffset;
+  @HostListener('window:scroll', ['$event.target']) // for window scroll events
+   scroll(e: any) {
+    let scroll = e.scrollingElement.scrollTop;
+    console.log("this is the scroll position", scroll)
+    if (scroll > this.currentPosition) {
+      console.log("scrollDown");
+   //    this.count = (+this.count + 1).toString();
+ this.prev = Number(this.count) + 1
+  this.count = this.prev.toString();
+  
+     
+     interval(1000).subscribe(x => {
+      // something
+      this.getProducts()
+      });
+      console.log(this.count)
+    } else {
+      console.log("scrollUp");
+    }
+    this.currentPosition = scroll;
+  }
   
 
 
